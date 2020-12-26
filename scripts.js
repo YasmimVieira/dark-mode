8360a1a39ef120ea05c33351ad66cb8a5dfd7206
Yasmim Barbosa Vieira <yasmim.vieira@aluno.faculdadeimpacta.com.br>
2020-12-26 01:07:29 -0300

    style: initial project

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const html = document.querySelector("html")
+const checkbox = document.querySelector("input[name=theme]")
+
+
+const initialColors = {
+    bg: //pegar esse estilo 
+}
+
+const changeColors = (colors) => {
+
+}
+
+
+checkbox.addEventListener("change", ({target}) => {
+    target.checked ? changeColors() : changeColors()
+})
